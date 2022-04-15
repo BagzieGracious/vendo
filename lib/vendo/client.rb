6889a9774b5775f3266baa3e44ea43e59572eee9
BagzieGracious <bagendadeogracious@gmail.com>
2022-04-15 07:53:27 +0300

    add client file and connection method

---
+++ lib/vendo/client.rb
@@ -0,0 +1,19 @@
+module Vendo
+    class Client
+        BASE_URL = "https://demo.getvendo.com/"
+
+        def connection(method, uri, params, headers)
+            url = URI.parse(BASE_URL + uri)
+            Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
+                headers['Content-Type'] = 'application/json'
+                if method == 'get'
+                    request = Net::HTTP::Get.new url
+                    request['Authorization'] = headers['Authorization']
+                    http.request request
+                else
+                    Net::HTTP.send(method, url, params.to_json, headers)
+                end
+            end
+        end
+    end    
+end
